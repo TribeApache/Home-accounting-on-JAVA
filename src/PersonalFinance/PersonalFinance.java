2928e9b6881cd3a58bd303c32d19d7a424c10d1e
Constantine Artemyev <ArtCostCorp@bk.ru>
2022-09-08 15:32:09 +0400

    ver.2

--- src/PersonalFinance/PersonalFinance.java
+++ src/PersonalFinance/PersonalFinance.java
@@ -1,5 +1,17 @@
 package PersonalFinance;
 
-public class PersonalFinance {
+import PersonalFinance.settings.Text;
 
+import java.awt.*;
+import java.io.File;
+
+public class PersonalFinance {
+    public static void main(String[] args) {
+        init();
+    }
+private static void init(){
+    Text.init();
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File())); // - вывести в отдельный класс, добавить путь к шрифту
+}
 }
--- src/PersonalFinance/settings/Text.java
+++ src/PersonalFinance/settings/Text.java
@@ -4,6 +4,26 @@ import java.util.HashMap;
 
 final public class Text {
     private static HashMap<String, String> data = new HashMap();
+    public static String get(String key){
+        return data.get(key);
+    }
+    public static String[] getMonths() {
+        String[] months = new String[12];
+        months[0] = get(JANUARY);
+        months[0] = get(FEBRUARY);
+        months[0] = get(MARCH);
+        months[0] = get(APRIL);
+        months[0] = get(MAY);
+        months[0] = get(JUNE);
+        months[0] = get(JULY);
+        months[0] = get(AUGUST);
+        months[0] = get(SEPTEMBER);
+        months[0] = get(OCTOBER);
+        months[0] = get(NOVEMBER);
+        months[0] = get(DECEMBER);
+        return months;
+
+    }
 
     public static void init() {
         data.put("PROGRAM_NAME", "Домашняя бухгалтерия");
@@ -11,5 +31,21 @@ final public class Text {
         data.put("MENU_EDIT", "Правка");
         data.put("MENU_VIEW", "Вид");
         data.put("PROGRAM_HELP", "Помощь");
+
+
+        data.put("JANUARY", "Январь");
+        data.put("FEBRUARY", "Февраль");
+        data.put("MARCH", "Март");
+        data.put("APRIL", "Апрель");
+        data.put("MAY", "Май");
+        data.put("JUNE", "Июнь");
+        data.put("JULY", "Июль");
+        data.put("AUGUST", "Август");
+        data.put("SEPTEMBER", "Сентябрь");
+        data.put("OCTOBER", "Октябрь");
+        data.put("NOVEMBER", "Ноябрь");
+        data.put("DECEMBER", "Декабрь");
+
     }
+
 }
